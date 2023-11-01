2d539c0e8b23699a024b231b69105c44cee4d671
Caleb Miller <caleb@mathmammoth.com>
2023-11-01 12:57:04 -0600

    asjdkwqjr

---
+++ README.md
@@ -0,0 +1,3 @@
+# rehype-break-links
+
+This package is a unified (rehype) plugin to use better line breaks for long URLs. Specifically, it applies to `a` tags where the content is the same as its `href`. See https://css-tricks.com/better-line-breaks-for-long-urls/
--- index.js
+++ index.js
@@ -10,12 +10,23 @@ export default function rehypeBreakLinks(options) {
         node.properties?.href &&
         node.children.length === 1 &&
         node.children[0].type === 'text' &&
-        node.children[0].value === node.properties.href
+        (node.children[0].value === node.properties.href ||
+          node.children[0].value ===
+            (() => {
+              try {
+                return decodeURIComponent(node.properties.href);
+              } catch (_) {}
+            })())
       ) {
         let prevNode = { type: 'text', value: '' };
-        const wbr = { type: 'element', tagName: 'wbr', properties: {}, children: [] };
+        const wbr = {
+          type: 'element',
+          tagName: 'wbr',
+          properties: {},
+          children: [],
+        };
         let nodes = [];
-        for (const char of node.properties.href.match(/\/\/|./g) || []) {
+        for (const char of node.children[0].value.match(/\/\/|./g) || []) {
           if (char === ':') {
             prevNode.value += char;
             nodes.push(prevNode, { ...wbr });
@@ -24,7 +35,12 @@ export default function rehypeBreakLinks(options) {
             nodes.push(prevNode, { ...wbr });
             prevNode = { type: 'text', value: char };
           } else if (/[=&]/.test(char) || char === '//') {
-            nodes.push(prevNode, { ...wbr }, { type: 'text', value: char }, { ...wbr });
+            nodes.push(
+              prevNode,
+              { ...wbr },
+              { type: 'text', value: char },
+              { ...wbr }
+            );
             prevNode = { type: 'text', value: '' };
           } else {
             prevNode.value += char;
@@ -33,8 +49,13 @@ export default function rehypeBreakLinks(options) {
         if (prevNode.value) nodes.push(prevNode);
         if (nodes[0]?.type === 'element') nodes.shift();
         if (nodes[nodes.length - 1]?.type === 'element') nodes.pop();
-        nodes = nodes.filter((x) => x.type === 'element' || (x.type === 'text' && x.value));
-        nodes = nodes.filter((x, i) => x.type !== 'element' || i === 0 || nodes[i - 1].type !== 'element');
+        nodes = nodes.filter(
+          (x) => x.type === 'element' || (x.type === 'text' && x.value)
+        );
+        nodes = nodes.filter(
+          (x, i) =>
+            x.type !== 'element' || i === 0 || nodes[i - 1].type !== 'element'
+        );
         node.children = nodes;
       }
     });
